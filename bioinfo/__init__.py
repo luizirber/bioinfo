718855d7c69880d9108c66308cf0e7bef30aa51c
Luiz Irber <irberlui@msu.edu>
2014-02-04 18:04:51 -0500

    Prettier output

--- bioinfo/__init__.py
+++ bioinfo/__init__.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 '''bioinfo
 
+Collection of scripts for bioinformatics problems.
+
 Usage:
   bioinfo bwa_coverage <reference> <bwa_alignment> <minmatch> <query> [--mapq=<n>]
   bioinfo -h | --help
@@ -16,17 +18,24 @@ Options:
 from __future__ import unicode_literals, print_function, division
 from docopt import docopt
 
+from .calc_bwa_cover import calc_bwa_coverage
+
+
 __version__ = "0.1.0"
 __author__ = "Luiz Irber"
 __license__ = "MIT"
 
 
+AVAILABLE_COMMANDS = {
+  'bwa_coverage': calc_bwa_coverage,
+}
+
+
 def main():
     '''Main entry point for the bioinfo CLI.'''
     args = docopt(__doc__, version=__version__)
-    if 'bwa_coverage' in args:
-        from .calc_bwa_cover import calc_bwa_coverage
 
+    if 'bwa_coverage' in args:
         calc_bwa_coverage(args['<reference>'],
                           args['<bwa_alignment>'],
                           int(args['<minmatch>']),
--- bioinfo/calc_bwa_cover.py
+++ bioinfo/calc_bwa_cover.py
@@ -11,28 +11,30 @@ longer than 'minmatch'.
 
 import pysam
 import screed
-import tqdm
+from tqdm import tqdm
 
 
 def calc_bwa_coverage(reference, bwa_alignments, min_match, query, min_mapq=30):
 
     # load in the query sequences into a list
     print "reading query"
-    query_seqs = set([record.name for record in screed.open(query)
-                      if len(record.sequence) >= minmatch])
-
-    print "creating empty lists"
+    query_seqs = set([record.name for record in tqdm(screed.open(query), leave=True)
+                      if len(record.sequence) >= min_match])
+    print
 
     # create empty lists representing the total number of bases in the
     # reference
+    print "creating empty lists"
     covs = {}
-    for record in tqdm(screed.open(reference)):
+    for record in tqdm(screed.open(reference), leave=True):
         covs[record.name] = [0] * len(record.sequence)
+    print
 
     # run through the BAM records in the query, and calculate how much of
     # the reference is covered by the query.
+    print 'building coverage'
     with pysam.Samfile(bwa_alignments, "rb") as samfile:
-        for record in tqdm(samfile):
+        for record in tqdm(samfile, leave=True):
 
             if record.qname not in query_seqs:
                 continue
@@ -47,22 +49,23 @@ def calc_bwa_coverage(reference, bwa_alignments, min_match, query, min_mapq=30):
             for pos_read, pos_ref in record.aligned_pairs:
                 if pos_ref:
                     cov[pos_ref] = 1
-
-    print ''
+    print
 
     # print out summary statistics for each of the reference.
     coved = 0
     total = 0
-    for name in covs:
+    print 'Summing stats'
+    for name in tqdm(covs, leave=True):
         this_cov = sum(covs[name])
         coved += this_cov
         total += len(covs[name])
         f = this_cov / float(len(covs[name]))
-        # print name, sum(covs[name]), len(covs[name]), f
+    print
 
+    print
     print 'total bases in reference:', total
     print 'total ref bases covered :', coved
     print 'fraction                :', coved / float(total)
     print 'reference               :', reference
-    print 'bwa alignment file      :', bwa_alignment
+    print 'bwa alignment file      :', bwa_alignments
     print 'query sequences         :', query
