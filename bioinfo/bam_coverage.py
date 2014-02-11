1e05a76dbc0905aa0d01a9b5b645e8e7f6709a6e
Luiz Irber <luiz.irber@gmail.com>
2014-02-11 15:23:59 -0500

    Add dependency check, tox in req-dev and print function

--- bioinfo/bam_coverage.py
+++ bioinfo/bam_coverage.py
@@ -9,30 +9,66 @@ covered by a BAM alignment file, for sequence in 'query.fa' that are
 longer than 'minmatch'.
 """
 
-import pysam
-import screed
-from tqdm import tqdm
+from __future__ import print_function, division
+import sys
+
+DEPENDENCIES = {
+    "pysam": False,
+    "screed": False,
+    "tqdm": False,
+}
+
+try:
+    import pysam
+    DEPENDENCIES['pysam'] = True
+except ImportError:
+    pass
+
+try:
+    import screed
+    DEPENDENCIES['screed'] = True
+except ImportError:
+    pass
+
+try:
+    from tqdm import tqdm
+    DEPENDENCIES['tqdm'] = True
+except ImportError:
+    pass
+
+
+def check_dependencies():
+    if all(DEPENDENCIES[d] for d in DEPENDENCIES):
+        return True
+
+    print("Missing dependencies, install before proceeding:")
+    for dep, installed in DEPENDENCIES.items():
+        if not installed:
+            print(dep)
+
+    sys.exit(1)
 
 
 def bam_coverage(reference, alignments, min_match, query, min_mapq=30):
+    check_dependencies()
 
     # load in the query sequences into a list
-    print "reading query"
+    print("reading query")
     query_seqs = set([r.name for r in tqdm(screed.open(query), leave=True)
                       if len(r.sequence) >= min_match])
-    print
+    print()
 
     # create empty lists representing the total number of bases in the
     # reference
-    print "creating empty lists"
+    print("creating empty lists")
     covs = {}
     for record in tqdm(screed.open(reference), leave=True):
         covs[record.name] = [0] * len(record.sequence)
-    print
+    print()
 
     # run through the BAM records in the query, and calculate how much of
     # the reference is covered by the query.
-    print 'building coverage'
+    print('building coverage')
     with pysam.Samfile(alignments, "rb") as samfile:
         for record in tqdm(samfile, leave=True):
 
@@ -49,31 +85,29 @@ def bam_coverage(reference, alignments, min_match, query, min_mapq=30):
             for pos_read, pos_ref in record.aligned_pairs:
                 if pos_ref:
                     cov[pos_ref] = 1
-    print
+    print()
 
     # print out summary statistics for each of the reference.
     coved = 0
     total = 0
-    print 'Summing stats'
+    print('Summing stats')
     for name in tqdm(covs, leave=True):
         this_cov = sum(covs[name])
         coved += this_cov
         total += len(covs[name])
         f = this_cov / float(len(covs[name]))
-    print
+    print()
 
-    print
-    print 'total bases in reference:', total
-    print 'total ref bases covered :', coved
-    print 'fraction                :', coved / float(total)
-    print 'reference               :', reference
-    print 'BAM alignment file      :', alignments
-    print 'query sequences         :', query
+    print()
+    print('total bases in reference:', total)
+    print('total ref bases covered :', coved)
+    print('fraction                :', coved / float(total))
+    print('reference               :', reference)
+    print('BAM alignment file      :', alignments)
+    print('query sequences         :', query)
 
 
 if __name__ == "__main__":
-    import sys
-
     reference = sys.argv[1]
     alignments = sys.argv[2]
     min_match = int(sys.argv[3])
