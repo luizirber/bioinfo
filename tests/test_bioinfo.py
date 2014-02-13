23c47930e9c2c156795ff9890be48cffa21955ac
Luiz Irber <luiz.irber@gmail.com>
2014-02-12 19:44:27 -0500

    Test improvements

--- tests/test_bioinfo.py
+++ tests/test_bioinfo.py
@@ -1,28 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import unittest
+
 from nose.tools import *  # PEP8 asserts
-from subprocess import check_output
+from scripttest import TestFileEnvironment
 
+class TestBamConverage(unittest.TestCase):
 
-class Testbioinfo(unittest.TestCase):
-
-    '''Unit tests for bioinfo.'''
+    '''Unit tests for bam_coverage.'''
 
     def setUp(self):
-        pass
+        self.env = TestFileEnvironment()
 
     def tearDown(self):
         pass
 
-    def test_echo(self):
-        '''An example test.'''
-        result = run_cmd("echo hello world")
-        assert_equal(result, "hello world\n")
-
-def run_cmd(cmd):
-    '''Run a shell command `cmd` and return its output.'''
-    return check_output(cmd, shell=True).decode('utf-8')
+    def test_arguments(self):
+        ret = self.env.run("bioinfo", "-h")
+        self.assertEquals(ret.returncode, 0)
+        self.assertIn('bam_coverage', ret.stdout)
 
 
 if __name__ == '__main__':
